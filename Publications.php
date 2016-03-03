0d5174c8ebc9f3e50975062284256e95f72a9352
Aidan Sawyer <aks5238@rit.edu>
2016-03-03 04:55:30 +0000

    expand shared code within header include

--- Publications.php
+++ Publications.php
@@ -12,22 +12,7 @@
     <div align="center" style="width: 100%" >
       <div class="container">
         
-        <div id="header" >
-          <table style="padding: 0; margin: 0; width: 100%;">
-            <tr style="vertical-align: middle;padding: 0; margin: 0;border:5px solid white; background-color: gray">
-              <td style="background-color: black;padding: 0; margin: 0;">
-                <img src="images/logo.png" width="80" height="60" alt="logo" />
-              </td>
-              <td>
-                <p style="padding-left:  10px; text-align: left; color:black; font-size: 40px; font-weight: bold; text-shadow: 0 1px 0 #999; " >Simulation For Education</p>
-              </td>
-            </tr>
-          </table>
-        </div>
-
-
         <?php include_once("header.html"); ?>
-
 
         <!-- publications -->
         <div id="page">
